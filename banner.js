504e1b83df9735d73ff2bd8faa5e8809a4e2a9ba
abdullah.waqas <abdullah.waqas@10pearls.local>
2020-07-06 22:39:59 +0500

    Added banner.js

--- banner.js
+++ banner.js
@@ -1,6 +1,6 @@
-// Used this file to add banner
 var banner = document.getElementById("banner");
 banner.innerText = "Banner";
 
 console.log(banner.textContent)
 alert('abc');
+// Used this file to add banner
