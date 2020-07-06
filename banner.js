c9395f2b3d5abd0d6d3e3c986164c9d710629d37
abdullah.waqas <abdullah.waqas@10pearls.local>
2020-07-06 23:04:24 +0500

    Fixed banner file

--- banner.js
+++ banner.js
@@ -4,3 +4,6 @@ banner.innerText = "Banner";
 console.log(banner.textContent)
 alert('abc');
 // Used this file to add banner
+// Used this file to add banner
+var banner = document.getElementById("banner");
+banner.innerText = "Banner";
