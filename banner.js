36543354cb299bc818e4b09391654d1bd504ba6f
abdullah.waqas <abdullah.waqas@10pearls.local>
2020-07-07 20:56:42 +0500

    updated header.js updated banner.js

--- banner.js
+++ banner.js
@@ -1,3 +1,5 @@
 // Used this file to add banner
 var banner = document.getElementById("banner");
 banner.innerText = "Banner";
+
+console.log(banner.textContent)
--- header.js
+++ header.js
@@ -3,3 +3,4 @@ var header = document.getElementById("header");
 header.classList.add("active");
 
 console.log(header);
+alert(header.innerHTML)
